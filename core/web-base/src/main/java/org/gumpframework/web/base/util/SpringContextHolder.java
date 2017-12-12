c95319cfdbfeaf84127df3271c483765638de197
daixinyu <xinyudai_ifox@icloud.com>
2017-12-12 18:12:38 +0800

    add spring

--- core/web-base/src/main/java/org/gumpframework/web/base/util/SpringContextHolder.java
+++ core/web-base/src/main/java/org/gumpframework/web/base/util/SpringContextHolder.java
@@ -6,6 +6,7 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * 以静态变量保存Spring ApplicatonContext
@@ -13,17 +14,22 @@ import org.springframework.context.ApplicationContextAware;
 @Slf4j
 public class SpringContextHolder implements ApplicationContextAware,DisposableBean {
     private static ApplicationContext applicationContext = null;
-
+    private static final String contextPath[]={"classpath:applicationContext*.xml"};
     @Override
     public void destroy() throws Exception {
-
+        applicationContext=null;
+        SpringContextHolder.applicationContext=null;
     }
-
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        if (PublicUtil.isNotEmpty(applicationContext)){
-            
+        if (PublicUtil.isNotEmpty(SpringContextHolder.applicationContext)){
+            log.warn("覆盖原有applicationContext:{}"+SpringContextHolder.applicationContext);
+        }
+        SpringContextHolder.applicationContext = applicationContext;
+    }
+    private static void assertContextInjected(){
+        if (PublicUtil.isEmpty(applicationContext)){
+            applicationContext = new ClassPathXmlApplicationContext(contextPath);
         }
     }
-
 }
